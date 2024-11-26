d6c6b28fbd34a57da905ce1c944c5773cbd0d694
Николай С <nicksrnk@gmail.com>
2024-11-27 01:20:24 +0500

    Для деплоя

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-let prod = false;
-export const host = prod ? '' : 'localhost';
-export const port = prod ? '' : ':8080/api';
+let prod = true;
+export const host = prod ? 'nickpyth-hakaton2024-b385.twc1.net' : 'localhost';
+export const port = prod ? '/api' : ':8080/api';
 export const secure = prod ? 'https' : 'http';
