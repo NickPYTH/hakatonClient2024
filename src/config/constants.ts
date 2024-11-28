2ae76f4fe44b719a0fdadf6a8449d8325d51b86d
Николай С <nicksrnk@gmail.com>
2024-11-28 15:31:41 +0500

    Для деплоя, новый урл апи

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 let prod = true;
-export const host = prod ? 'nickpyth-hakaton2024-b385.twc1.net' : 'localhost';
+export const host = prod ? 'nickpyth-hakaton2024-0367.twc1.net' : 'localhost';
 export const port = prod ? '/api' : ':8080/api';
 export const secure = prod ? 'https' : 'http';
-export const dateTimeFormat = "DD-MM-YYYY HH:mm";
+export const dateTimeFormat3 = "DD-MM-YYYY HH:mm";
